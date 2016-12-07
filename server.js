0a45c0286f4dbb9a669f6934d9bf96f58ea50936
Joshua Lanese <froastj@gmail.com>
2016-12-06 18:55:46 -0800

    maximumRank is optional

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 var port = process.env.PORT || 8080;
 var settings = require('./settings.json');
 var key = settings.key;
-var maximumRank = settings.maximumRank;
+var maximumRank = settings.maximumRank || 255;
 
 app.set('env', 'production');
 
