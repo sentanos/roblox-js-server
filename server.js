b22a85fa6e3c6b5f529522ee430255f3413b0c64
Sanjay <Sanjay-B@users.noreply.github.com>
2017-09-09 13:52:50 -0400

    Add display port number to console
    - Allows users to see the port number when server is running

--- server.js
+++ server.js
@@ -493,7 +493,7 @@ function login () {
 setInterval(login, 86400000);
 login().then(function () {
   app.listen(port, function () {
-    console.log('Listening');
+    console.log('Listening on Port : ' + port);
   });
 })
 .catch(function (err) {
